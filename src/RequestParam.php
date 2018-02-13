6101fea6a3e7ead1e1cb44fe1eb961c67cca431d
Michal Kluczek <michal.kluczek@liteweb.io>
2018-02-13 16:30:24 +0100

    Initial push

---
+++ src/RequestParam.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Liteweb\Timber\TimberApi;
+
+final class RequestParam
+{
+	public const JSON = 'application/json';
+
+	public const PLAIN = 'plain/text';
+}
---
+++ src/TimberApi.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Liteweb\Timber\TimberApi;
+
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+
+class TimberApi
+{
+    private $http_client;
+    private $token;
+    private $content_type = RequestParam::PLAIN;
+
+    function __construct()
+    {
+        $this->http_client = new \GuzzleHttp\Client([
+            'base_uri' => 'https://logs.timber.io'
+        ]);
+    }
+
+    public function setAuthToken($token)
+    {
+        $this->token = $token;
+    }
+
+    public function sendRawLogLine(string $line): \GuzzleHttp\Psr7\Response
+    {
+        return $this->postRequest('/frames', $line);
+    }
+
+    public function sendJsonLogLine(string $json): \GuzzleHttp\Psr7\Response
+    {
+        $this->content_type = RequestParam::JSON;
+
+        return $this->postRequest('/frames', $json);
+    }
+
+    private function postRequest(string $path, string $body): \GuzzleHttp\Psr7\Response
+    {
+        return $this->http_client->post($path,  $this->prepareRequestOptions($body));
+    }
+
+    private function prepareRequestOptions(string $body = null)
+    {
+        return [
+            'body'    => $body,
+            'headers' => $this->prepareRequestHeaders()
+        ];
+    }
+
+    private function prepareRequestHeaders()
+    {
+        return [
+            'Authorization' => 'Basic ' . base64_encode($this->token),
+            'Content-Type'  => $this->content_type,
+        ];
+    }
+}
